845fd96852055208f3c7313fba35584a866721e0
Eline Verbon <eline.verbon@nedap.com>
2020-12-02 11:24:27 +0100

    Day 1, problem 2

--- src/day1/Solution.java
+++ src/day1/Solution.java
@@ -10,8 +10,11 @@ import java.util.stream.IntStream;
 public final class Solution {
 	
 	List<Integer> input = new ArrayList<>();
-	int firstNumber = 0;
-	int secondNumber = 0;
+	int firstNumberSetOf2 = 0;
+	int secondNumberSetOf2 = 0;
+	int firstNumberSetOf3 = 0;
+	int secondNumberSetOf3 = 0;
+	int thirdNumberSetOf3 = 0;
 	
 	
 	public static void main(String[] args) {
@@ -23,9 +26,17 @@ public final class Solution {
 	private void solvePuzzle() {
 		convertInputToList();
 		findTwoNumbers();
-		System.out.println("Number 1: " + firstNumber);
-		System.out.println("Number 1: " + secondNumber);
-		System.out.println("Solution: " + firstNumber * secondNumber);
+		System.out.println("Solution two numbers");
+		System.out.println("Number 1: " + firstNumberSetOf2);
+		System.out.println("Number 2: " + secondNumberSetOf2);
+		System.out.println("Solution: " + firstNumberSetOf2 * secondNumberSetOf2);
+		
+		findThreeNumbers();
+		System.out.println("Solution two numbers");
+		System.out.println("Number 1: " + firstNumberSetOf3);
+		System.out.println("Number 2: " + secondNumberSetOf3);
+		System.out.println("Number 3: " + thirdNumberSetOf3);
+		System.out.println("Solution: " + firstNumberSetOf3 * secondNumberSetOf3 * thirdNumberSetOf3);
 	}
 	
 	/** Converts to input to a list of integers */
@@ -50,10 +61,30 @@ public final class Solution {
 					int option1 = input.get(n);
 					int option2 = input.get(x);
 					if (option1 + option2 == 2020) {
-						firstNumber = option1;
-						secondNumber = option2;
+						firstNumberSetOf2 = option1;
+						secondNumberSetOf2 = option2;
 					}
 				}
+			});
+		});
+	}
+	
+	/** loop through input and find three numbers that add up to 2020. */
+	private void findThreeNumbers() {
+		IntStream.range(0, input.size()).forEach(x -> {
+			IntStream.range(0, input.size()).forEach(y -> {
+				IntStream.range(0, input.size()).forEach(z -> {
+					if(x != y && x != z && y != z) {
+						int option1 = input.get(x);
+						int option2 = input.get(y);
+						int option3 = input.get(z);
+						if (option1 + option2 + option3 == 2020) {
+							firstNumberSetOf3 = option1;
+							secondNumberSetOf3 = option2;
+							thirdNumberSetOf3 = option3;
+						}
+					}
+				});
 			});
 		});
 	}
